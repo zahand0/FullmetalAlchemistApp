88e62b9e23abe946a4bd8f065abf9de8f84403b1
Sergey <mulichev@gmail.com>
2022-10-24 16:10:29 +0300

    added HeroRemoteKey entity and heroRemoteKeyDao fun

--- app/src/main/java/com/example/fullmetalalchemistapp/data/local/FullmetalAlchemistDatabase.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/data/local/FullmetalAlchemistDatabase.kt
@@ -3,13 +3,20 @@ package com.example.fullmetalalchemistapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.fullmetalalchemistapp.data.local.dao.HeroDao
+import com.example.fullmetalalchemistapp.data.local.dao.HeroRemoteKeyDao
 import com.example.fullmetalalchemistapp.domain.model.Hero
+import com.example.fullmetalalchemistapp.domain.model.HeroRemoteKey
 import com.example.models.MilitaryRank
 import com.example.models.Species
 
-@Database(entities = [Hero::class, MilitaryRank::class, Species::class], version = 1)
-abstract class FullmetalAlchemistDatabase: RoomDatabase() {
+@Database(
+    entities = [Hero::class, MilitaryRank::class, Species::class, HeroRemoteKey::class],
+    version = 1
+)
+abstract class FullmetalAlchemistDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
+
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 
 }
