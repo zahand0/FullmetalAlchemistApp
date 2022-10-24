4b454d5eb9d8e3ccdf249619661ff5cc34e5e5b8
Sergey <mulichev@gmail.com>
2022-10-24 19:49:45 +0300

    added splash screen

--- app/src/main/java/com/example/fullmetalalchemistapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/navigation/NavGraph.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.fullmetalalchemistapp.util.Constants
+import com.example.fullmetalalchemistapp.presentation.screens.splash.SplashScreen
 import com.example.fullmetalalchemistapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
@@ -16,7 +16,7 @@ fun SetupNavGraph(navController: NavHostController) {
         startDestination = Screen.Splash.route
     ) {
         composable(route = Screen.Splash.route) {
-
+            SplashScreen(navController = navController)
         }
         composable(route = Screen.Welcome.route) {
 
