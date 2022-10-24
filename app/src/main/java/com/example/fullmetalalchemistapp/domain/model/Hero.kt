ab5789ded854f44b42f3b46092e3e13ac892b49f
Sergey <mulichev@gmail.com>
2022-10-24 15:38:26 +0300

    added model

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/domain/model/Hero.kt
@@ -0,0 +1,23 @@
+package com.example.fullmetalalchemistapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.fullmetalalchemistapp.util.Constants.HERO_DATABASE_TABLE
+import com.example.models.MilitaryRank
+import com.example.models.Species
+
+@Entity(tableName = HERO_DATABASE_TABLE)
+data class Hero(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val image: String,
+    val memorableQuotes: List<String>,
+    val alias: String,
+    val rating: Double,
+    val about: String,
+    val species: Species,
+    val militaryRank: MilitaryRank,
+    val abilities: List<String>,
+    val weapons: List<String>
+)
