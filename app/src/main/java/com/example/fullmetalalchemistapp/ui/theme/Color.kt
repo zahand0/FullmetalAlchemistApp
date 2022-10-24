10f7a497fe70e5c0c7da63759d0d39b093f29a08
Sergey <mulichev@gmail.com>
2022-10-24 19:51:52 +0300

    added colors

--- app/src/main/java/com/example/fullmetalalchemistapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/ui/theme/Color.kt
@@ -6,3 +6,19 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Pink50 = Color(0xFFFCE4EC)
+
+val DeepPurple200 = Color(0xFFb39ddb)
+val DeepPurple400 = Color(0xFF7e57c2)
+val DeepPurple700 = Color(0xFF7b1fa2)
+
+val Indigo50 = Color(0xFFe8eaf6)
+val Indigo100 = Color(0xFFc5cae9)
+
+val LightBlue50 = Color(0xFFe1f5fe)
+val LightBlue100 = Color(0xFFb3e5fc)
+
+
+val Grey900 = Color(0xFF212121)
+
