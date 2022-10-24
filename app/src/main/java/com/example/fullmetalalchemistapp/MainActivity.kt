204d72bda725c6c2d4c32088911bed56f01381e9
Sergey <mulichev@gmail.com>
2022-10-24 14:15:46 +0300

    initialized navController

--- app/src/main/java/com/example/fullmetalalchemistapp/MainActivity.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/MainActivity.kt
@@ -10,13 +10,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.fullmetalalchemistapp.ui.theme.FullmetalAlchemistAppTheme
+import com.example.fullmetalalchemistapp.navigation.SetupNavGraph
 
 class MainActivity : ComponentActivity() {
+    
+    private lateinit var navController: NavHostController
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            FullmetalAlchemistAppTheme {
 
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
         }
     }
 }
