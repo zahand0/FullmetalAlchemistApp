8bf41410a393a4783d40d3a8e25e60ba9946b38f
Sergey <mulichev@gmail.com>
2022-10-25 00:17:28 +0300

    implemented pager screen, added pager indicator and finish button

--- app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,18 +1,28 @@
 package com.example.fullmetalalchemistapp.presentation.screens.welcome
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.fullmetalalchemistapp.R
 import com.example.fullmetalalchemistapp.domain.model.OnBoardingPage
-import com.example.fullmetalalchemistapp.ui.theme.backgroundColorBrush
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
+import com.example.fullmetalalchemistapp.ui.theme.*
+import com.google.accompanist.pager.*
 
 @ExperimentalPagerApi
 @Composable
@@ -28,14 +38,34 @@ fun WelcomeScreen(navController: NavHostController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.backgroundColorBrush)
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center
     ) {
         HorizontalPager(
             count = pages.size,
             state = pagerState,
-            verticalAlignment = Alignment.Top
-        ) { page ->
-            PagerScreen(onBoardingPage = pages[page])
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier.weight(10f)
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+        
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            activeColor = MaterialTheme.colors.primary,
+            inactiveColor = MaterialTheme.colors.inactiveIndicatorColor,
+            indicatorWidth = PAGING_INDICATOR_WIDTH,
+            spacing = PAGING_INDICATOR_SPACING,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .weight(1f)
+        )
+        FinishButton(
+            modifier = Modifier
+                .weight(1f),
+            pagerState = pagerState
+        ) {
+            
         }
     }
 }
@@ -44,5 +74,99 @@ fun WelcomeScreen(navController: NavHostController) {
 fun PagerScreen(
     onBoardingPage: OnBoardingPage
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = stringResource(R.string.on_boarding_image)
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = onBoardingPage.title,
+            color = MaterialTheme.colors.onBackground,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = onBoardingPage.description,
+            color = MaterialTheme.colors.onBackground.copy(alpha = 0.6f),
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = EXTRA_LARGE_PADDING)
+                .padding(top = SMALL_PADDING)
+        )
+    }
 }
+
+@ExperimentalPagerApi
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState: PagerState,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = EXTRA_LARGE_PADDING)
+            .padding(horizontal = EXTRA_LARGE_PADDING),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(
+            modifier = Modifier
+                .fillMaxWidth(),
+            visible = pagerState.currentPage == pagerState.pageCount - 1
+        ) {
+            Button(
+                onClick = onClick,
+                shape = CircleShape,
+                modifier = Modifier
+                    .shadow(
+                        elevation = BUTTON_ELEVATION,
+                        shape = CircleShape
+                    )
+            ) {
+                Text(text = "Finish")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstOnBoardScreenPreview() {
+        Column(modifier = Modifier.fillMaxSize()) {
+            PagerScreen(onBoardingPage = OnBoardingPage.First)
+        }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondOnBoardScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Second)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThirdOnBoardScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Third)
+    }
+}
+
+
+
