f55771092c522801a5da0bdbdb8f119b7988153f
Sergey <mulichev@gmail.com>
2022-10-24 20:29:27 +0300

    added welcome screen

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/welcome/WelcomeScreen.kt
@@ -0,0 +1,48 @@
+package com.example.fullmetalalchemistapp.presentation.screens.welcome
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.fullmetalalchemistapp.domain.model.OnBoardingPage
+import com.example.fullmetalalchemistapp.ui.theme.backgroundColorBrush
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+
+@ExperimentalPagerApi
+@Composable
+fun WelcomeScreen(navController: NavHostController) {
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third
+    )
+
+    val pagerState = rememberPagerState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.backgroundColorBrush)
+    ) {
+        HorizontalPager(
+            count = pages.size,
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { page ->
+            PagerScreen(onBoardingPage = pages[page])
+        }
+    }
+}
+
+@Composable
+fun PagerScreen(
+    onBoardingPage: OnBoardingPage
+) {
+
+}
