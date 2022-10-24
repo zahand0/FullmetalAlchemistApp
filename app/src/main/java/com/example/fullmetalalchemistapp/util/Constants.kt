05ea6c0bc11d6ec3605d590596e8ce582819e458
Sergey <mulichev@gmail.com>
2022-10-24 17:02:23 +0300

    added fullmetal alchemist database name

--- app/src/main/java/com/example/fullmetalalchemistapp/util/Constants.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/util/Constants.kt
@@ -2,6 +2,8 @@ package com.example.fullmetalalchemistapp.util
 
 object Constants {
 
+    const val FULLMETAL_ALCHEMIST_DATABASE = "fullmetal_alchemist_database"
+
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
     const val HERO_DATABASE_TABLE = "hero_table"
