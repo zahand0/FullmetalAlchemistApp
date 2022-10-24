f7f9e1f65233a5eb91074e424f598fa7b89194b9
Sergey <mulichev@gmail.com>
2022-10-25 00:15:12 +0300

    added dimensions holder

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/ui/theme/Dimens.kt
@@ -0,0 +1,13 @@
+package com.example.fullmetalalchemistapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val EXTRA_LARGE_PADDING = 32.dp
+val LARGE_PADDING = 20.dp
+val MEDIUM_PADDING = 16.dp
+val SMALL_PADDING = 10.dp
+
+val PAGING_INDICATOR_WIDTH = 12.dp
+val PAGING_INDICATOR_SPACING = 8.dp
+
+val BUTTON_ELEVATION = 4.dp
