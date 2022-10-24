974516a7a1ad4f6a3f7f1080dfff27b25f4bc0bb
Sergey <mulichev@gmail.com>
2022-10-24 20:29:07 +0300

    boarding page info sealed class

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/domain/model/OnBoardingPage.kt
@@ -0,0 +1,27 @@
+package com.example.fullmetalalchemistapp.domain.model
+
+import androidx.annotation.DrawableRes
+import com.example.fullmetalalchemistapp.R
+
+sealed class OnBoardingPage(
+    @DrawableRes
+    val image: Int,
+    val title: String,
+    val description: String
+) {
+    object First: OnBoardingPage(
+        image = R.drawable.greetings,
+        title = "Greeting",
+        description = "Are you Fullmetal Alchemist fan? Because if you are then we have a great news for you!"
+    )
+    object Second: OnBoardingPage(
+        image = R.drawable.explore,
+        title = "Explore",
+        description = "Find your favorite heroes and learn some of the things that you don't know about."
+    )
+    object Third: OnBoardingPage(
+        image = R.drawable.power,
+        title = "Power",
+        description = "Check out your hero's power and see how much are they strong comparing to others."
+    )
+}
