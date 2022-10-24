eb8da9669adec4cd46bd645f830f6a9e3f9a0a66
Sergey <mulichev@gmail.com>
2022-10-24 16:38:04 +0300

    changed enum to data class

--- app/src/main/java/com/example/fullmetalalchemistapp/domain/model/MilitaryRank.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/domain/model/MilitaryRank.kt
@@ -1,14 +1,6 @@
 package com.example.models
 
-enum class MilitaryRank(
+data class MilitaryRank(
     val rankName: String,
     val img: String
-) {
-FuhrerPresident("Führer-President", "/images/ranks/fuhrerPresident.jpg"),
-MajorGeneral("Major General", "/images/ranks/majorGeneral.jpg"),
-Colonel("Colonel", "/images/ranks/colonel.jpg"),
-LieutenantColonel("Lieutenant Colonel", "/images/ranks/lieutenantColonel.jpg") ,
-Major("Major", "/images/ranks/major.jpg") ,
-FirstLieutenant("First Lieutenant", "/images/ranks/firstLieutenant.jpg"),
-Civilian("Civilian", "")
-}
+)
