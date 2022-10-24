fb7d1dfc3d38d19f1e45d6d88e13a7609e213aa6
Sergey <mulichev@gmail.com>
2022-10-24 16:41:04 +0300

    removed military rank and species classes from entity declaration, added DatabaseConverter type converter

--- app/src/main/java/com/example/fullmetalalchemistapp/data/local/FullmetalAlchemistDatabase.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/data/local/FullmetalAlchemistDatabase.kt
@@ -2,17 +2,18 @@ package com.example.fullmetalalchemistapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.fullmetalalchemistapp.data.local.dao.HeroDao
 import com.example.fullmetalalchemistapp.data.local.dao.HeroRemoteKeyDao
 import com.example.fullmetalalchemistapp.domain.model.Hero
 import com.example.fullmetalalchemistapp.domain.model.HeroRemoteKey
 import com.example.models.MilitaryRank
-import com.example.models.Species
 
 @Database(
-    entities = [Hero::class, MilitaryRank::class, Species::class, HeroRemoteKey::class],
+    entities = [Hero::class, HeroRemoteKey::class],
     version = 1
 )
+@TypeConverters(DatabaseConverter::class)
 abstract class FullmetalAlchemistDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
