c5571de1527bff567570c50c7fee2e1192f253f5
Sergey <mulichev@gmail.com>
2022-10-25 14:51:40 +0300

    fixed search screen root

--- app/src/main/java/com/example/fullmetalalchemistapp/navigation/Screen.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/navigation/Screen.kt
@@ -6,7 +6,7 @@ sealed class Screen(val route: String) {
     object Splash: Screen("splash_screen")
     object Welcome: Screen("welcome_screen")
     object Home: Screen("home_screen")
-    object Search: Screen("home_screen")
+    object Search: Screen("search_screen")
     object Details: Screen("details_screen/{$DETAILS_ARGUMENT_KEY}") {
         fun passHeroId(heroId: Int): String {
             return "details_screen/$heroId"
