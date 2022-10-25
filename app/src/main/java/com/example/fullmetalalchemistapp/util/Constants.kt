967caf5117fb1799296e560f5465fe41674f0be5
Sergey <mulichev@gmail.com>
2022-10-25 12:15:54 +0300

    added preferences constants

--- app/src/main/java/com/example/fullmetalalchemistapp/util/Constants.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/util/Constants.kt
@@ -2,12 +2,13 @@ package com.example.fullmetalalchemistapp.util
 
 object Constants {
 
-    const val FULLMETAL_ALCHEMIST_DATABASE = "fullmetal_alchemist_database"
-
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
+    const val FULLMETAL_ALCHEMIST_DATABASE = "fullmetal_alchemist_database"
     const val HERO_DATABASE_TABLE = "hero_table"
-
     const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
+
+    const val PREFERENCES_NAME = "fma_preferences"
+    const val PREFERENCES_KEY = "on_boarding_completed"
 
 }
