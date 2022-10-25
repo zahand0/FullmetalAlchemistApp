98f3c7d6e3a642fa7d6336ea001c32cc0013f2b2
Sergey <mulichev@gmail.com>
2022-10-25 12:39:17 +0300

    added On Boarding use cases

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/domain/use_cases/save_onboarding/SaveOnBoardingUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.fullmetalalchemistapp.domain.use_cases.save_onboarding
+
+import com.example.fullmetalalchemistapp.data.repository.Repository
+
+class SaveOnBoardingUseCase(
+    private val repository: Repository
+) {
+    suspend operator fun invoke(completed: Boolean) {
+        repository.saveOnBoardingState(completed = completed)
+    }
+}
