0f6adc9be916718dcb35712225a1d3d87f172493
Sergey <mulichev@gmail.com>
2022-10-25 12:16:48 +0300

    provided DataStoreOperations

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/di/RepositoryModule.kt
@@ -0,0 +1,25 @@
+package com.example.fullmetalalchemistapp.di
+
+import android.content.Context
+import com.example.fullmetalalchemistapp.data.pref.DataStoreOperationsImpl
+import com.example.fullmetalalchemistapp.domain.repository.DataStoreOperations
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStoreOperations(
+        @ApplicationContext context: Context
+    ): DataStoreOperations {
+        return DataStoreOperationsImpl(context = context)
+    }
+
+}
