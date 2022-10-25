424d10a4289fd9fe0e688a39df72f784fb5e9261
Sergey <mulichev@gmail.com>
2022-10-25 17:55:47 +0300

    implemented getAllHeroes fun

--- app/src/main/java/com/example/fullmetalalchemistapp/data/repository/Repository.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/data/repository/Repository.kt
@@ -1,12 +1,20 @@
 package com.example.fullmetalalchemistapp.data.repository
 
+import androidx.paging.PagingData
+import com.example.fullmetalalchemistapp.domain.model.Hero
 import com.example.fullmetalalchemistapp.domain.repository.DataStoreOperations
+import com.example.fullmetalalchemistapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private val dataStore: DataStoreOperations
+    private val dataStore: DataStoreOperations,
+    private val remote: RemoteDataSource
 ) {
+
+    fun getAllHeroes(): Flow<PagingData<Hero>> {
+        return remote.getAllHeroes()
+    }
 
     suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.saveOnBoardingState(completed)
