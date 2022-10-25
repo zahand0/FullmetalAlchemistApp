d9a77bee2dc2b281736ff1fd5e4a6c40b81f7dd9
Sergey <mulichev@gmail.com>
2022-10-25 14:52:33 +0300

    added top bar

--- app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/home/HomeScreen.kt
@@ -1,12 +1,17 @@
 package com.example.fullmetalalchemistapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 
 @Composable
 fun HomeScreen(
     navController: NavHostController
 ) {
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }) {
+    }
 
 }
