7b1c55b2c4a9012c96050e896cd3c4cae0ff35cf
Sergey <mulichev@gmail.com>
2022-10-25 13:15:42 +0300

    added navigation to Welcome/Home screen

--- app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/splash/SplashScreen.kt
@@ -10,9 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -21,15 +19,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.fullmetalalchemistapp.R
+import com.example.fullmetalalchemistapp.navigation.Screen
 import com.example.fullmetalalchemistapp.ui.theme.FullmetalAlchemistAppTheme
 import com.example.fullmetalalchemistapp.ui.theme.backgroundColorBrush
 
 @Composable
 fun SplashScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    splashViewModel: SplashViewModel = hiltViewModel()
 ) {
+
+    val onBoardingCompleted by splashViewModel.onBoardingCompleted.collectAsState()
 
     val degrees = remember {
         Animatable(0f)
@@ -43,6 +46,12 @@ fun SplashScreen(
                 delayMillis = 200
             )
         )
+        navController.popBackStack()
+        if (onBoardingCompleted) {
+            navController.navigate(Screen.Home.route)
+        } else {
+            navController.navigate(Screen.Welcome.route)
+        }
     }
 
     Splash(degrees = degrees.value)
