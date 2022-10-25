d4a8d02410bddc9ad8cf9872eec104bec6428326
Sergey <mulichev@gmail.com>
2022-10-25 13:16:16 +0300

    added Home screen

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/home/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.fullmetalalchemistapp.presentation.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(
+    navController: NavHostController
+) {
+
+}
