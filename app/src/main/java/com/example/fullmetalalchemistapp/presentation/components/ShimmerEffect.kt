b4209f537fac9bc8c3746fe6d2dc65959731bcda
Sergey <mulichev@gmail.com>
2022-10-29 20:05:27 +0300

    implemeted shimmer effect on hero cards

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/components/ShimmerEffect.kt
@@ -0,0 +1,120 @@
+package com.example.fullmetalalchemistapp.presentation.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fullmetalalchemistapp.ui.theme.*
+
+@Composable
+fun ShimmerEffect() {
+    LazyColumn(
+        contentPadding = PaddingValues(SMALL_PADDING),
+        verticalArrangement = Arrangement.spacedBy(SMALL_PADDING)
+    ) {
+        items(count = 2) {
+            AnimatedShimmerItem()
+        }
+    }
+}
+
+@Composable
+fun AnimatedShimmerItem() {
+    val transition = rememberInfiniteTransition()
+    val animateFloat by transition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0.3f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 800,
+                easing = FastOutLinearInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    ShimmerItem(alpha = animateFloat)
+}
+
+@Composable
+fun ShimmerItem(alpha: Float) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(HERO_ITEM_HEIGHT),
+        color = if (isSystemInDarkTheme())
+            Color.Black else ShimmerLightGray,
+        shape = RoundedCornerShape(LARGE_PADDING)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(MEDIUM_PADDING),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Surface(
+                modifier = Modifier
+                    .alpha(alpha)
+                    .fillMaxWidth(0.5f)
+                    .height(NAME_PLACEHOLDER_HEIGHT),
+                color = if (isSystemInDarkTheme())
+                    ShimmerDarkGray else ShimmerMediumGray,
+                shape = RoundedCornerShape(SMALL_PADDING)
+            ) {}
+            Spacer(modifier = Modifier.padding(SMALL_PADDING))
+            repeat(3) {
+                Surface(
+                    modifier = Modifier
+                        .alpha(alpha)
+                        .fillMaxWidth()
+                        .height(ABOUT_PLACEHOLDER_HEIGHT),
+                    color = if (isSystemInDarkTheme())
+                        ShimmerDarkGray else ShimmerMediumGray,
+                    shape = RoundedCornerShape(SMALL_PADDING)
+                ) {}
+                Spacer(modifier = Modifier.padding(EXTRA_SMALL_PADDING))
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                repeat(5) {
+                    Surface(
+                        modifier = Modifier
+                            .alpha(alpha)
+                            .size(RATING_PLACEHOLDER_HEIGHT),
+                        color = if (isSystemInDarkTheme())
+                            ShimmerDarkGray else ShimmerMediumGray,
+                        shape = RoundedCornerShape(SMALL_PADDING)
+                    ) {}
+                    Spacer(modifier = Modifier.padding(SMALL_PADDING))
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ShimmerItemPreview() {
+    FullmetalAlchemistAppTheme {
+
+        AnimatedShimmerItem()
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun ShimmerItemDarkPreview() {
+    FullmetalAlchemistAppTheme() {
+
+        AnimatedShimmerItem()
+    }
+}
