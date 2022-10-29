d434d412add87b219f5f8ec360189283772d303f
Sergey <mulichev@gmail.com>
2022-10-29 20:03:29 +0300

    added Shimmer colors

--- app/src/main/java/com/example/fullmetalalchemistapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/ui/theme/Color.kt
@@ -8,25 +8,7 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-
 val Teal200 = Color(0xFF03DAC5)
-
-val Pink50 = Color(0xFFFCE4EC)
-
-val DeepPurple200 = Color(0xFFb39ddb)
-val DeepPurple400 = Color(0xFF7e57c2)
-val DeepPurple700 = Color(0xFF7b1fa2)
-
-val Indigo50 = Color(0xFFe8eaf6)
-val Indigo100 = Color(0xFFc5cae9)
-
-val LightBlue50 = Color(0xFFe1f5fe)
-val LightBlue100 = Color(0xFFb3e5fc)
-
-val Grey800 = Color(0xFF323232)
-val Grey900 = Color(0xFF212121)
-val Grey950 = Color(0xFF202020)
-val Grey990 = Color(0xFF1B1B1B)
 
 val LightCream100 = Color(0xfff9f9f7)
 val LightCream300 = Color(0xffefeee8)
@@ -35,6 +17,10 @@ val LightCream400 = Color(0xffeae8e0)
 val LightGrey = Color(0xFFD8D8D8)
 val DarkGrey = Color(0xFF1F1F1F)
 val DarkGreyLighter = Color(0xFF2A2A2A)
+
+val ShimmerLightGray = Color(0xFFF1F1F1)
+val ShimmerMediumGray = Color(0xFFE3E3E3)
+val ShimmerDarkGray = Color(0xFF1D1D1D)
 
 val Colors.backgroundColorBrush
     @Composable
