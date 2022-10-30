f52d1010a21cc052bc1e4ee255fef4d58f296b11
Sergey <mulichev@gmail.com>
2022-10-30 18:02:34 +0300

    added search widget

--- app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchScreen.kt
@@ -2,19 +2,37 @@ package com.example.fullmetalalchemistapp.presentation.screens.search
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.focus.FocusRequester
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
 @Composable
 fun SearchScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    searchViewModel: SearchViewModel = hiltViewModel()
 ) {
+    val searchQuery by searchViewModel.searchQuery
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     Scaffold(
         topBar = {
             SearchWidget(
-                text = "",
-                onTextChange = {},
+                text = searchQuery,
+                onTextChange = {
+                    searchViewModel.updateSearchQuery(query = it)
+                },
                 onSearchClicked = {},
-                onCloseClicked = {}
+                onCloseClicked = {
+                    navController.popBackStack()
+                },
+                focusRequester = focusRequester
             )
         }
     ) {
