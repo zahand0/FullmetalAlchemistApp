dee0b0c565a72d44aa6878035506016795b3b1ff
Sergey <mulichev@gmail.com>
2022-10-30 18:03:15 +0300

    implemented SearchViewModel

---
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.fullmetalalchemistapp.presentation.screens.search
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    private val _searchQuery = mutableStateOf("")
+    val searchQuery: State<String> = _searchQuery
+
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
+}
