f1dcf6ce130cf399e531039348dd33d56c246e82
Sergey <mulichev@gmail.com>
2022-10-30 17:26:45 +0300

    added SearchScreen call

--- app/src/main/java/com/example/fullmetalalchemistapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/navigation/NavGraph.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavType
 import com.google.accompanist.navigation.animation.composable
 import androidx.navigation.navArgument
 import com.example.fullmetalalchemistapp.presentation.screens.home.HomeScreen
+import com.example.fullmetalalchemistapp.presentation.screens.search.SearchScreen
 import com.example.fullmetalalchemistapp.presentation.screens.splash.SplashScreen
 import com.example.fullmetalalchemistapp.presentation.screens.welcome.WelcomeScreen
 import com.example.fullmetalalchemistapp.util.Constants.DETAILS_ARGUMENT_KEY
@@ -31,7 +32,7 @@ fun SetupNavGraph(navController: NavHostController) {
             HomeScreen(navController = navController)
         }
         composable(route = Screen.Search.route) {
-
+            SearchScreen(navController = navController)
         }
         composable(
             route = Screen.Details.route,
