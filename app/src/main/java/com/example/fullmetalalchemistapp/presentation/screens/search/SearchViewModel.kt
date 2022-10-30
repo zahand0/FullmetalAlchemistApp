cc49e9461e565f8f2523df49463c7abee45ec6a8
Sergey <mulichev@gmail.com>
2022-10-30 18:22:05 +0300

    added searchHeroes fun

--- app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchViewModel.kt
@@ -3,18 +3,40 @@ package com.example.fullmetalalchemistapp.presentation.screens.search
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.fullmetalalchemistapp.domain.model.Hero
+import com.example.fullmetalalchemistapp.domain.use_cases.UseCases
+import com.example.fullmetalalchemistapp.domain.use_cases.search_heroes.SearchHeroesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-
+    private val useCases: UseCases
 ) : ViewModel() {
 
     private val _searchQuery = mutableStateOf("")
     val searchQuery: State<String> = _searchQuery
 
+    private val _searchedHeroes = MutableStateFlow<PagingData<Hero>>(PagingData.empty())
+    val searchedHeroes: StateFlow<PagingData<Hero>> = _searchedHeroes
+
     fun updateSearchQuery(query: String) {
         _searchQuery.value = query
+    }
+
+    fun searchHeroes(query: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            useCases.searchHeroesUseCase(query = query).cachedIn(viewModelScope).collect {
+                _searchedHeroes.value = it
+            }
+        }
     }
 }
