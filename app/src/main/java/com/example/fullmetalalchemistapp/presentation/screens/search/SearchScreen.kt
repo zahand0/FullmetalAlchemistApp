13bc1c1f8573576b3abe25faf6f02adf7600e2ee
Sergey <mulichev@gmail.com>
2022-10-30 20:02:03 +0300

    added ListContent

--- app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/fullmetalalchemistapp/presentation/screens/search/SearchScreen.kt
@@ -1,14 +1,17 @@
 package com.example.fullmetalalchemistapp.presentation.screens.search
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.fullmetalalchemistapp.presentation.common.ListContent
 
 @Composable
 fun SearchScreen(
@@ -39,7 +42,11 @@ fun SearchScreen(
                 focusRequester = focusRequester
             )
         }
-    ) {
-
+    ) { padding ->
+        ListContent(
+            modifier = Modifier.padding(padding),
+            heroes = heroes,
+            navController = navController
+        )
     }
 }
